3a3007c90979de7559672f5d062de1e6e9373020
Brian Teachman <me@briant.me>
2013-09-16 11:17:04 -0700

    Added email method to Application/IndexController.

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -51,13 +51,47 @@ class IndexController extends AbstractActionController
             if ($form->isValid()) {
 
                 $validData = $form->getData();
-                // $this->flashMessenger()->addMessage(serialize($validData));
-                // $this->sendContactForm($validData);
+                $this->flashMessenger()->addMessage(serialize($validData));
+                $this->sendContactForm($validData);
 
                 $this->redirect()->toUrl('home');
             }
         }
 
         return new ViewModel(array('form' => $form));
+    }
+
+    /**
+     * Multi-part sendmail method
+     * 
+     * @param  string[] $info Array containing elements of the message.
+     */
+    protected function sendContactForm($info)
+    {
+        $meta = array(
+           'from' => $info['email'],
+           'from_name' => $info['name'],
+           'to' => 'me@briant.me',
+           'to_name' => 'Energy Specialist',
+           'subject' => 'Sacred-Energy Contact Form',
+        );
+
+        $text_body = <<<TEXTBODY
+{$info['name']}: \n{$info['message']}.
+TEXTBODY;
+
+        $html_body = <<<HTMLBODY
+<html><head></head><body>
+<h1>{$info['name']} say's:</h1>
+<p>{$info['message']}</p>
+</body></html>
+HTMLBODY;
+
+        try {
+            // defined in TWeb\Controller\AbstractController
+            $this->sendMultiPartMail($meta, $html_body, $text_body);
+        } catch (Exception $e) {
+            //log it or something
+        }
     }
 }
