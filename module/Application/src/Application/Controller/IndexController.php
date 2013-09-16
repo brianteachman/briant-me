69c78fb1c06e9c38c9393cb8d080080502494ec1
Brian Teachman <me@briant.me>
2013-09-16 11:21:07 -0700

    Added email method to Application/IndexController.

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -72,8 +72,8 @@ class IndexController extends AbstractActionController
            'from' => $info['email'],
            'from_name' => $info['name'],
            'to' => 'me@briant.me',
-           'to_name' => 'Energy Specialist',
-           'subject' => 'Sacred-Energy Contact Form',
+           'to_name' => 'BrianT',
+           'subject' => 'www.briant.me Contact Form',
         );
 
         $text_body = <<<TEXTBODY
@@ -93,5 +93,53 @@ HTMLBODY;
         } catch (Exception $e) {
             //log it or something
         }
+    }
+
+    /**
+     * Multi-part sendmail method
+     *
+     * $head = array(
+     *     'from' => '',
+     *     'from_name' => '',
+     *     'to' => '',
+     *     'to_name' => '',
+     *     'subject' => '',
+     * )
+     * 
+     * @param string[] $head      Array containing mailto info.
+     * @param string   $html_body Plain text email message.
+     * @param string   $text_body HTML email message.
+     */
+    protected function sendMultiPartMail($meta, $html_body="", $text_body="")
+    {
+        if (!isset($meta['to']) || !isset($meta['from'])) {
+            throw new \InvalidArguementException("'to' and 'from' must be set.");
+        }
+
+        $mail = new Mail\Message();
+        $mail->setEncoding("UTF-8");
+        
+        $mail->setFrom($meta['from'], $meta['from_name'])
+             ->addTo($meta['to'], $meta['to_name'])
+             ->setSubject($meta['subject']);
+
+        if (isset($meta['reply_to'])) {
+            $mail->addReplyTo($meta['reply_to'], $meta['reply_to_name']);
+        }
+
+        $text = new MimePart($text_body);
+        $text->type = "text/plain";
+
+        $html = new MimePart($html_body);
+        $html->type = "text/html";
+
+        $body = new MimeMessage();
+        $body->setParts(array($text, $html));
+
+        $mail->setBody($body);
+        $mail->getHeaders()->get('content-type')->setType('multipart/alternative');
+
+        $transport = new Mail\Transport\Sendmail();
+        $transport->send($mail);
     }
 }
