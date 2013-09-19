4d5478a1476e740af3e0c9c707d5137113167647
Brian Teachman <me@briant.me>
2013-09-18 23:42:31 -0700

    Copy and design updates.

---
+++ pushassets.sh
@@ -0,0 +1,5 @@
+# move assets to web root
+cp -ru ./public/js/* ../../public_html/js/*
+cp -ru ./public/css/* ../../public_html/css/*
+cp -ru ./public/img/* ../../public_html/img/*
+cp -ru ./public/.htaccess ../../public_html/.htaccess
